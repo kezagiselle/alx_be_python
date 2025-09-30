58fd06d78f34355e26e52b8dc0e19eb251b68430
kezagiselle <kezagiselle776@gmail.com>
2025-09-30 09:46:37 +0200

    more exercises

---
+++ control-flow/EvenOdd.py
@@ -0,0 +1,8 @@
+def checkEven(num):
+    if num % 2 == 0:
+        print("Is even")
+    else:
+        print("Odd")
+        
+checkEven(4)
+checkEven(7)
---
+++ control-flow/practisesToDo.py
@@ -0,0 +1,16 @@
+# Practice Exercises
+# Exercise 1: Creating and Using Modules
+
+# Instructions:
+
+# Create a custom Python module named calculator.py that contains functions for basic arithmetic operations (addition, subtraction, multiplication, division).
+# Create functions add, subtract, multiply, and divide in the calculator.py module.
+# Import the calculator module into a separate Python script main.py and use its functions to perform arithmetic operations on numbers like 5 and 3.
+# Exercise 2: Exploring Python Libraries
+
+# Instruction:
+
+# Use the requests library to fetch weather information from an online weather API (e.g., weatherapi.
+# Install the requests library using pip if it’s not already installed pip install requests.
+# Write a Python script weather.py to use the requests.get method to fetch weather data from the API.
+# Print out relevant weather information (e.g., temperature, weather description) from the API response.
